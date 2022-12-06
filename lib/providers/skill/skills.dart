9773951d6e229fc60ed9178f40c64ae29a75fc9d
Erick Namukolo <erickmndev@gmail.com>
2022-12-06 13:51:41 +0200

    added functionality for uploading skill icons

--- lib/providers/skill/skills.dart
+++ lib/providers/skill/skills.dart
@@ -1,16 +1,28 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:portfolio_admin/constants/constants.dart';
 
 class Skills with ChangeNotifier {
-  static Future<void> addSkill(String name, String description) async {
-    Map<String, dynamic> dataMap = {
-      'name': name,
-      'des': description,
-      'hidden': false,
-      'img': null,
-      'date': DateTime.now().toIso8601String(),
-    };
+  static Future<void> addSkill(
+      File icon, String name, String description) async {
     try {
+      UploadTask uploadTask;
+      final iconPath = 'skill-icons/${name}';
+      final ref = firebaseStorage.ref().child(iconPath);
+      uploadTask = ref.putFile(icon);
+      final snapshot = await uploadTask.whenComplete(() {});
+      final iconUrl = await snapshot.ref.getDownloadURL();
+
+      Map<String, dynamic> dataMap = {
+        'name': name,
+        'des': description,
+        'hidden': false,
+        'img': iconUrl,
+        'date': DateTime.now().toIso8601String(),
+      };
+
       await adminRef.child('skills').push().set(dataMap);
     } catch (e) {
       throw e;
--- lib/screens/skills/add_skill_screen.dart
+++ lib/screens/skills/add_skill_screen.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:portfolio_admin/constants/text.dart';
 import 'package:portfolio_admin/providers/skill/skills.dart';
 import 'package:portfolio_admin/widgets/custom_button.dart';
+import 'package:portfolio_admin/widgets/custom_loading.dart';
 import 'package:portfolio_admin/widgets/custom_text_field.dart';
 import 'package:portfolio_admin/widgets/custom_toast.dart';
 import 'package:portfolio_admin/widgets/cutsom_app_bar.dart';
+
+import '../../constants/colors.dart';
 
 class AddSkillScreen extends StatefulWidget {
   static const routeName = '/add-skill-screen';
@@ -19,6 +26,24 @@ class _AddSkillScreenState extends State<AddSkillScreen> {
   final _name = TextEditingController();
   final _des = TextEditingController();
   bool _isLoading = false;
+  final ImagePicker _picker = ImagePicker();
+  File? _icon;
+
+  Future<void> getDp() async {
+    XFile? pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage == null) return;
+    File imageTemp = File(pickedImage.path);
+    setState(() {
+      _icon = imageTemp;
+    });
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
 
   @override
   void dispose() {
@@ -30,11 +55,42 @@ class _AddSkillScreenState extends State<AddSkillScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: _isLoading ? CustomLoading() : null,
       appBar: CustomAppBar(title: 'Add Skill', showLeading: true),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         child: Column(
           children: [
+            GestureDetector(
+              onTap: () async {
+                getDp();
+              },
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.all(10.0),
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  color: kSecondaryColor,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: _icon == null
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                          Text('Icon', style: kBodyTextStyleWhite),
+                        ],
+                      )
+                    : Image.file(
+                        _icon!,
+                      ),
+              ),
+            ),
             CustomTextField(
               hint: 'Name',
               preIcon: Iconsax.code_15,
@@ -48,13 +104,18 @@ class _AddSkillScreenState extends State<AddSkillScreen> {
             const Spacer(),
             CustomButton(
               btnText: 'Add Skill',
+              isLoading: _isLoading,
               click: () async {
                 try {
-                  await Skills.addSkill(_name.text, _des.text);
+                  setState(() => _isLoading = true);
+                  await Skills.addSkill(_icon!, _name.text, _des.text);
+                  setState(() => _isLoading = false);
+                  Navigator.of(context).pop();
                 } catch (e) {
                   BotToast.showCustomNotification(
                       toastBuilder: (context) =>
                           CustomToast(message: e.toString(), type: 'error'));
+                  setState(() => _isLoading = false);
                 }
               },
             ),
